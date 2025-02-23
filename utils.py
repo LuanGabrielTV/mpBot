8a61fe07257f68cb2cdb1bdc92c262ccbe6ed5cf
Leonam Teixeira de Vasconcelos <leonam.teixeira.vasconcelos@gmail.com>
2025-02-22 22:46:14 -0300

    feat(garbage-collector): upgrading clean up strategy

--- utils.py
+++ utils.py
@@ -1,14 +1,11 @@
-import glob
 import os
+import shutil
 
-from constants import download_path, temp_reddit_folder
+from constants import download_path
 
 def clean_up_files():
     if not os.path.exists(download_path):
         raise "Download path doesn't exist"
-    for file in glob.glob(download_path + '*'):
-        print(f'Deleting file={file}...')
-        os.remove(file)
-    if os.path.isdir(os.path.join(download_path,temp_reddit_folder)):
-        os.rmdir(temp_reddit_folder)
+    shutil.rmtree(download_path)
+    os.mkdir(download_path)
     
